8be315bf664dfd0532e7c9fd9ff51200a21b65db
von-solano <a1890523@adelaide.edu.au>
2025-06-19 06:06:47 +0000

    [automatic commit] 2025-06-19 06:06:46 UTC 129.127.202.17

--- part1/database.sql
+++ part1/database.sql
@@ -30,8 +30,8 @@ CREATE TABLE book_authors(
     book_id INT,
     author_id INT,
     PRIMARY KEY (book_id, author_id),
-    FOREIGN KEY (book_id)  REFERENCES books(book_id),
-    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+    FOREIGN KEY (book_id)  REFERENCES books(book_id) ON DELETE CASCADE,
+    FOREIGN KEY (author_id) REFERENCES authors(author_id) ON DELETE CASCADE
 );
 
 CREATE TABLE transactions(

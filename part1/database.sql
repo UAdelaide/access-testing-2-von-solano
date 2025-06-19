e6f397b997c408c899783295dd86f7b6f5eab031
von-solano <a1890523@adelaide.edu.au>
2025-06-19 05:55:43 +0000

    [automatic commit] 2025-06-19 05:55:43 UTC 129.127.202.17

--- part1/database.sql
+++ part1/database.sql
@@ -39,5 +39,8 @@ CREATE TABLE transactions(
     seller_id INT NOT NULL,
     book_id INT NOT NULL,
     price DECIMAL(10,2),
-    transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY buyer_id REFERENCES users(user_id),
+    FOREIGN KEY seller_id REFERENCES users(user_id),
+    FOREIGN KEY book_id REFERENCES books(book_id),
 );

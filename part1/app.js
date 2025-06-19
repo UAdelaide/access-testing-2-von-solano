c1278949ef7205795e3a3fd87f5a09831f6c4b95
von-solano <a1890523@adelaide.edu.au>
2025-06-19 11:11:19 +0000

    [automatic commit] 2025-06-19 11:11:19 UTC 120.88.144.42

--- part1/app.js
+++ part1/app.js
@@ -7,7 +7,11 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 const mysql = require('mysql2');
-const db = mysql.createPool()
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'user',
+    
+})
 
 var app = express();
 

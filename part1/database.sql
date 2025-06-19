1fee1974f7b0a317c8ef70f1543f59b37e3a8bee
von-solano <a1890523@adelaide.edu.au>
2025-06-19 05:53:00 +0000

    [automatic commit] 2025-06-19 05:53:00 UTC 129.127.202.17

--- part1/database.sql
+++ part1/database.sql
@@ -29,6 +29,6 @@ CREATE TABLE book_authors(
     book_id INT,
     author_id INT,
     PRIMARY KEY (book_id, author_id),
-    FOREIGN KEY (book_id)  REFERENCES book(book_id),
+    FOREIGN KEY (book_id)  REFERENCES books(book_id),
     FOREIGN KEY (author_id) REFERENCES authors(author_id)
 );

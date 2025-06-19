8ef2335169126b8f1ee7a7fd42ece3bc0adb8753
von-solano <a1890523@adelaide.edu.au>
2025-06-19 05:46:48 +0000

    [automatic commit] 2025-06-19 05:46:48 UTC 129.127.202.17

--- part1/database.sql
+++ part1/database.sql
@@ -13,7 +13,7 @@ CREATE TABLE users(
 
 CREATE TABLE books(
     book_id INT AUTO_INCREMENT PRIMARY KEY,
-    seller_id INT AUTO_INCREMENT REFERENCES usres(user_id),
+    seller_id INT AUTO_INCREMENT REFERENCES users(user_id),
     title VARCHAR(100) NOT NULL,
     ISBN VARCHAR(20) NOT NULL,
     price DECIMAL(20,8),

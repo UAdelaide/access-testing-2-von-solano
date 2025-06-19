6f32130f96975288847d764666a8edcf3389f81b
von-solano <a1890523@adelaide.edu.au>
2025-06-19 05:43:44 +0000

    [automatic commit] 2025-06-19 05:43:44 UTC 129.127.202.17

--- part1/database.sql
+++ part1/database.sql
@@ -6,7 +6,7 @@ CREATE TABLE users(
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     location VARCHAR(100)
 );

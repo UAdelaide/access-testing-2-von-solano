3b2489f7abd0c8746d58efe58bc54dcb6a6f8c8a
von-solano <a1890523@adelaide.edu.au>
2025-06-19 11:10:38 +0000

    [automatic commit] 2025-06-19 11:10:38 UTC 120.88.144.42

--- part1/app.js
+++ part1/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const mysql = require()
+
 var app = express();
 
 // VIEW ENGINE SETUP
@@ -17,8 +19,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-const db = db.connection
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);

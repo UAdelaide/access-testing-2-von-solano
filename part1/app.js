703cc3d0c073d7637394a8f94af3f4ac65edb8d1
von-solano <a1890523@adelaide.edu.au>
2025-06-19 06:54:15 +0000

    [automatic commit] 2025-06-19 06:54:15 UTC 129.127.202.17

--- part1/app.js
+++ part1/app.js
@@ -8,6 +8,10 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// VIEW ENGINE SETUP
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

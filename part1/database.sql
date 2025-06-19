550caf26e8ad96384261656fd0e6877c8c5513c2
von-solano <a1890523@adelaide.edu.au>
2025-06-19 05:24:50 +0000

    [automatic commit] 2025-06-19 05:24:50 UTC 129.127.202.17

--- part1/database.sql
+++ part1/database.sql
@@ -4,5 +4,8 @@ USE textbook_marketplace;
 -- USERS TABLE
 CREATE TABLE users(
     user_id INT PRIMARY KEY,
-    first_name VARCHAR(50)
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(100),
+
 );

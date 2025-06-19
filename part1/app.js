1dc3ad21530fe6f023aa6da231505bda6f715c34
von-solano <a1890523@adelaide.edu.au>
2025-06-19 06:31:58 +0000

    [automatic commit] 2025-06-19 06:31:58 UTC 129.127.202.17

--- part1/app.js
+++ part1/app.js
@@ -21,4 +21,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.get('/buyer', (req, res) => {
+    res.render('buyer');
+});
+
 module.exports = app;

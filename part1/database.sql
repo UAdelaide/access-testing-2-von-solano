9d2157b2d819221574e9ce1523f7dc3f408d485c
von-solano <a1890523@adelaide.edu.au>
2025-06-19 05:48:29 +0000

    [automatic commit] 2025-06-19 05:48:29 UTC 129.127.202.17

--- part1/database.sql
+++ part1/database.sql
@@ -8,7 +8,6 @@ CREATE TABLE users(
     last_name VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    location VARCHAR(100)
 );
 
 CREATE TABLE books(
